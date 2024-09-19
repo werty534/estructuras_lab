39f40fb16b58b96f5ac3d18a931495bc266b5263
werty534 <tomas.canos@edu.uah.es>
2024-09-19 19:32:36 +0200

    Ejemplos hasta el 4

--- ejemplo01/main.cpp
+++ ejemplo01/main.cpp
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	int x;
+	std::cout << "Escriba un número: ";
+	std::cin >> x;
+	std::cout << "El número introducido es: " << x << "\n";
+	
 	return 0;
 }
---
+++ ejemplo02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int x, y, z;
+	cout << "Introduzca el primer número: ";
+	cin >> x;
+	cout << "Introduzca el segundo número: ";
+	cin >> y;
+	z = x + y;
+	cout << "La suma " << x << " mas " << y << " es igual a " << z << "." << endl;
+	
+	return 0;
+}
