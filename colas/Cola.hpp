3d6ccc996ef38b94ab5c324c58ebbae1d2634945
werty534 <tomas.canos@edu.uah.es>
2024-10-03 23:02:19 +0200

    Colas hechas

---
+++ colas/Cola.hpp
@@ -0,0 +1,17 @@
+#include "NodoCola.hpp"
+
+class Cola
+{
+public:
+	Cola();
+	~Cola();
+	
+	void insertar(int v);
+	int eliminar();
+	void mostrar();
+	int verPrimero();
+
+private:
+	pnodoCola primero, ultimo;
+	int longitud;
+};
